a72b1231e91386114f56ab1443ec19567feadd49
Josh Whitney <sh.tney@gmail.com>
2025-05-09 22:59:42 +1200

    new logger, better docker file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,16 @@
-import * as express from 'express';
-import { Express, Request, Response } from 'express';
+import * as express from "express";
+import type { Express, Request, Response } from "express";
+import { ConsoleLogger } from "./logger";
 
 export const app: Express = express();
 
-app.get('/', (_req: Request, res: Response) => {
-    console.log(`INC: ${_req.ip}`)
-    res.send('Hello World');
+app.get("/", (req: Request, res: Response) => {
+	const logger = new ConsoleLogger();
+	logger.info('here');
+
+	res.send(`Hello World ${req.ip}`);
 });
 
-if (require.main === module) {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-}
-
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
